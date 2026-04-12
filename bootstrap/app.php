02eb40b515a0ff8002be79048ce46f7830705fc7
e-mandy <andymfrd02@gmail.com>
2026-04-12 23:33:27 +0100

    chore(architecture): Opened the api configuration for laravel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,6 +26,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 5e55ab6 (chore(architecture): Opened the api configuration for laravel)
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
 >>>>>>> 17ff392 (feat(architecture): Set up the backend code base structure)
--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
+<<<<<<< HEAD
 })->middleware('auth:sanctum');
+=======
+})->middleware('auth:sanctum');
+>>>>>>> 5e55ab6 (chore(architecture): Opened the api configuration for laravel)
