64bc45404a2b4d4c25548ebe395014a9894f6ceb
e-mandy <andymfrd02@gmail.com>
2026-04-12 23:33:27 +0100

    chore(architecture): Opened the api configuration for laravel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,9 +6,15 @@ use Illuminate\Foundation\Application;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
+<<<<<<< HEAD
         web: __DIR__ . '/../routes/web.php',
         api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
+=======
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+>>>>>>> 5e55ab6 (chore(architecture): Opened the api configuration for laravel)
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
