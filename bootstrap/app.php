d0c6a965d412cbb18e8f81cb1ebc019aa8faf517
e-mandy <andymfrd02@gmail.com>
2026-04-12 23:33:27 +0100

    chore(architecture): Opened the api configuration for laravel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
 >>>>>>> 17ff392 (feat(architecture): Set up the backend code base structure)
         health: '/up',
