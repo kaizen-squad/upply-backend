837eb5e8473e262aa0fe91e077a9e511c3f63dfa
e-mandy <andymfrd02@gmail.com>
2026-04-12 23:24:52 +0100

    feat(architecture): Set up the backend code base structure

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,10 +7,10 @@ use Illuminate\Foundation\Application;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-<<<<<<< HEAD
         web: __DIR__ . '/../routes/web.php',
         api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
+<<<<<<< HEAD
 =======
 <<<<<<< HEAD
 use Illuminate\Foundation\Application;
@@ -29,6 +29,8 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
 >>>>>>> 5e55ab6 (chore(architecture): Opened the api configuration for laravel)
 >>>>>>> 64bc454 (chore(architecture): Opened the api configuration for laravel)
+=======
+>>>>>>> 6f54ca6 (feat(architecture): Set up the backend code base structure)
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
