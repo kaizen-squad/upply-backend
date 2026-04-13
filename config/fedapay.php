10e5afcecd30fad6dc0ebf6b7dea3b994b485388
Eunock <izumishinishi7@gmail.com>
2026-04-13 02:20:00 +0100

    feat(api) Implement FedaPay transaction verification function

---
+++ config/fedapay.php
@@ -0,0 +1,8 @@
+<?php
+    return [
+        'secret_key' => env('FEDAPAY_SECRET_KEY'),
+        'public_key' => env('FEDAPAY_PUBLIC_KEY'),
+        'webhook_key' => env('FEDAPAY_WEBHOOK_KEY'),
+        'environment' => env('FEDAPAY_ENVIRONMENT')
+    ];
+
