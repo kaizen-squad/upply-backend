bff0fbfeeeefafb99c144cf93e130b26cd534d77
Eunock <izumishinishi7@gmail.com>
2026-04-13 04:48:13 +0100

    feat(api) Implement creation realease function in the TransactionService

--- app/Services/Fedapay/TransactionService.php
+++ app/Services/Fedapay/TransactionService.php
@@ -2,6 +2,8 @@
     namespace App\Services\FedapayTransactionService;
 
 use App\Models\TransactionLogs;
+use App\Models\Transaction;
+use App\Models\User;
 use App\Services\FedapayServices\FedapayService;
 use Illuminate\Support\Facades\Auth;
 
@@ -54,7 +56,62 @@ use Illuminate\Support\Facades\Auth;
             }
 
         //Fonction qui permet maintenant de faire le versemnt directe sur le numero de l'utilisateur
-        public function release(){
+        public function release($clientId, $prestataireId,$amount, $currency,$description){
+            $fedapayService = new FedapayService();
 
+            try{
+                if(!$clientId && !$prestataireId){
+                    return response()->json([
+                        'success' => false,
+                        'message' => 'clientid and prestataireId required'
+                    ]);
+                }
+
+                //Recherche de la transaction où le client et le prestataire corresponde
+                $transaction = Transaction::where('client_id', $clientId and 'prestataire_id', $prestataireId);
+
+                if(!$transaction){
+                    return response()->json([
+                        'success' => false,
+                        'message' => 'Transaction not found'
+                    ], 404);
+                }
+
+                //Recherche des infos du prestataire
+                $prestataireInfo = User::where('user_id', $prestataireId);
+
+                $data = [
+                    'amount' => $amount,
+                    'currency' => ['iso'=>$currency],
+                    'description' => $description,
+                    'customer' => [
+                        'firstname' => $prestataireInfo['firstname'],
+                        'lastname' => $prestataireInfo['lastname'],
+                        'email' => $prestataireInfo['email'],
+                        'phone_number' => [
+                            'number' => $prestataireInfo['phone'],
+                            'country' => $prestataireInfo['country']
+                        ]
+                    ]
+                ];
+
+                //Lancer maintenant le payout
+                $payout = $fedapayService->payout($data);
+
+                //Verification si le payout s'est bien passé
+                if($payout['success'] === true){
+                    $payout['data'].sendNow();
+                }
+
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Payout successfully',
+                ], 200);
+            }catch(\Exception $e){
+                return response()->json([
+                    'success' => false,
+                    'error' => $e->getMessage(),
+                ], 500);
+            }
         }
     }
