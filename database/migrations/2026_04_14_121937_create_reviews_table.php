77294877d3a7ecdf1187b61cc7c23aeec562493f
e-mandy <andymfrd02@gmail.com>
2026-04-14 12:56:38 +0000

    feat(reviews): Set up the reviews migration

---
+++ database/migrations/2026_04_14_121937_create_reviews_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("reviewer_id")->constrained("users")->cascadeOnDelete();
+            $table->morphs("reviewee");
+
+            $table->smallInteger("rating")->min();
+            $table->text("comment")->nullable();
+            $table->timestamps();
+        });
+
+        DB::statement("ALTER TABLE reviews ADD CONSTRAINT rating_range CHECK(rating >= 1 AND rating <= 5)");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
