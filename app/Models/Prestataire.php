7ecd73f3e553df1ffce09895be0b5eaa863d0acd
OKWUDIAFOR Wesley <okwuwesley@gmail.com>
2026-04-14 12:20:04 +0100

    feat- Database service & Redis service setup

---
+++ app/Models/Prestataire.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Prestataire extends Model
+{
+    /** @use HasFactory<\Database\Factories\PrestataireFactory> */
+    use HasFactory;
+}
