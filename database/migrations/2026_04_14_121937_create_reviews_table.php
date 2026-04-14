afd29a9261fde99bf66677f328730e62790eace5
e-mandy <andymfrd02@gmail.com>
2026-04-14 14:41:22 +0000

    fix(migrations): Changed id foreign key to uuid foreign key

--- database/migrations/2026_04_14_121937_create_reviews_table.php
+++ database/migrations/2026_04_14_121937_create_reviews_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignUuid("reviewer_id")->constrained("users")->cascadeOnDelete();
+            $table->morphs("reviewee");
 
             $table->uuid("id")->primary();
             $table->foreignUuid("reviewer_id")->constrained("users")->cascadeOnDelete();
