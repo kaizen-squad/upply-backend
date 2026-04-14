7ae3193ce5414552d5bd511c97ded760787f09d7
e-mandy <andymfrd02@gmail.com>
2026-04-14 03:05:45 +0000

    feat(tasks): Set up the task migration

---
+++ database/migrations/2026_04_14_021121_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->foreignId("client_id")->constrained("users")->cascadeOnDelete();
+            
+            $table->string("title");
+            $table->text("description");
+            $table->integer("budget");
+            $table->date("deadline");
+            $table->enum("status", ["opened", "on_going", "delivered", "validated"]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
