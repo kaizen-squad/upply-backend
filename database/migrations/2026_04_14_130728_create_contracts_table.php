5ee758261a79b2b2be07c23e19e05b3ff0385901
e-mandy <andymfrd02@gmail.com>
2026-04-14 13:11:33 +0000

    feat(contracts): Added contracts migration for application table

--- database/migrations/2026_04_14_130728_create_contracts_table.php
+++ database/migrations/2026_04_14_130728_create_contracts_table.php
@@ -12,8 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('contracts', function (Blueprint $table) {
+<<<<<<< HEAD
             $table->uuid("id")->primary();
             $table->foreignUuid("application_id")->unique()->constrained()->cascadeOnDelete();
+=======
+            $table->uuid();
+            $table->foreignId("application_id")->constrained()->cascadeOnDelete();
+>>>>>>> 9733822 (feat(contracts): Added contracts migration for application table)
 
             $table->timestamps();
         });
