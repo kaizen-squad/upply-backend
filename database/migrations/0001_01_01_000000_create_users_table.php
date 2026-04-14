f5b2ff42cfb8190f91ab52b66b80ba00b70a3566
e-mandy <andymfrd02@gmail.com>
2026-04-14 19:40:34 +0000

    fix(migrations): Added id column name - Removed min constraint on review rating column

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,13 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->uuid()->primary();
+            $table->uuid("id")->primary();
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->enum("role", ['client', 'prestataire']);
+            $table->enum("role", ['client', 'prestataire'])->default('client');
             $table->string("phone")->nullable();
-            $table->decimal("rating_avg", 3, 2);
+            $table->decimal("rating_avg", 3, 2)->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
@@ -34,7 +34,7 @@ return new class extends Migration
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
+            $table->foreignUuid('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
