c8e3ccf217f572404109f94c922fd36a52cf66ce
e-mandy <andymfrd02@gmail.com>
2026-04-18 19:37:13 +0000

    feat(applications): Set up the DTO for the listing of task applications

--- app/DTOs/Application/ApplicationListForTaskDTO.php
+++ app/DTOs/Application/ApplicationListForTaskDTO.php
@@ -2,15 +2,19 @@
 
 namespace App\DTOs\Application;
 
+use App\Http\Requests\Application\ApplicationListForTaskRequest;
+
 readonly class ApplicationListForTaskDTO{
     public function __construct(
         public string $task_id
     ){}
 
-    public static function fromRequest(): self
+    public static function fromRequest(ApplicationListForTaskRequest $request): self
     {
+        $data = $request->validated();
+
         return new self(
-            task
+            task_id: $data['task_id']
         );
     }
 }
--- app/Http/Requests/Application/ApplicationListForTaskRequest.php
+++ app/Http/Requests/Application/ApplicationListForTaskRequest.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Requests\Application;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ApplicationListForTaskRequest extends FormRequest
