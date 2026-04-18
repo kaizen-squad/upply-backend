27c4d5d61e4a152665b0d95bc15e92088a931bc9
e-mandy <andymfrd02@gmail.com>
2026-04-18 16:56:52 +0000

    chore: Added the DomainException for error occurs by domain violation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Middleware\IsAuthenticated;
+use App\Exceptions\ApiException;
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -19,6 +21,14 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })
-    ->create();
+        $exceptions->render(function (Throwable $e, Request $request){
+            if($request->is('api/*')){
+                $response = ApiException::render($e);
+
+                return response()->json(
+                    collect($response),
+                    $response['status']
+                );
+            }
+        });
+    })->create();
