41c44dece4b6ed170f1b5b15294ab378b80a13f5
e-mandy <andymfrd02@gmail.com>
2026-04-18 08:08:47 +0000

    feat(tasks): Set up the deletion of task

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use App\Traits\AuthToken;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -15,7 +16,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasUuids, HasFactory, Notifiable, HasApiTokens, AuthToken;
+    use HasUuids, HasFactory, Notifiable, HasApiTokens, AuthToken, SoftDeletes;
 
     public $incrementing = false;
     protected $keyType = 'string';
