a4f32d2d097d70817090905748438d6caf3e68e5
e-mandy <andymfrd02@gmail.com>
2026-04-18 17:26:27 +0000

    feat(application): Set up the Application store request

---
+++ app/Http/Requests/Application/ApplicationStoreRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\Application;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ApplicationStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'message' => ['required', 'string'],
+            'task_id' => ['required', 'exists:tasks,id']
+        ];
+    }
+}
