54a773fe831c6e01f9094b668481afc8f84a1fc0
e-mandy <andymfrd02@gmail.com>
2026-04-18 05:59:47 +0000

    feat(tasks): Set up the controller create method

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\DTOs\Task\TaskStoreDTO;
+use App\Http\Requests\Task\TaskStoreRequest;
 use App\Services\TaskService;
 use Exception;
 
@@ -18,14 +20,34 @@ class TaskController{
                 "success" => true,
                 "data" => $response,
                 "message" => "All Tasks"
-            ]);
+            ], 200);
 
         }catch(Exception $e){
             return response()->json([
                 "success" => false,
                 "message" => $e->getMessage()
-            ]);
+            ], 400);
         }
+    }
 
+    public function create(TaskStoreRequest $request){
+        $user = $request->user();
+
+        $taskData = TaskStoreDTO::fromRequest($request);
+
+        try{
+            $response = $this->service->create($user, $taskData);
+
+            return response()->json([
+                "success" => true,
+                "data" => $response,
+                "message" => "Task created successfully"
+            ], 200);
+        }catch(Exception $e){
+            return response()->json([
+                "success" => false,
+                "message" => $e->getMessage()
+            ], 403);
+        }
     }
 }
