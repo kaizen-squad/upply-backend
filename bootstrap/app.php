17d704e6ad5a1b177f03e32c85e62d7cb00532d4
e-mandy <andymfrd02@gmail.com>
2026-04-18 16:56:52 +0000

    chore: Added the DomainException for error occurs by domain violation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,14 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })
-    ->create();
+        $exceptions->render(function (Throwable $e, Request $request){
+            if($request->is('api/*')){
+                $response = ApiException::render($e);
+
+                return response()->json(
+                    collect($response),
+                    $response['status']
+                );
+            }
+        });
+    })->create();
