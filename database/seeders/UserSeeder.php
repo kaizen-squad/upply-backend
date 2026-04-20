2310ea81430537942066f086683b6759264928d5
e-mandy <andymfrd02@gmail.com>
2026-04-20 04:33:08 +0000

    fix(seeders): Added the number of prestataire and task, so that it will respect the requirement document

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Enums\TaskStatus;
 use App\Models\Task;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -14,16 +16,22 @@ class UserSeeder extends Seeder
     public function run(): void
     {
 
-        // This command will create 5 "prestataire".
+        // This command will create 5 "client" and 10 "prestataire".
         User::factory()
-            ->count(16)
+            ->count(10)
             ->prestataire()
             ->create();
 
         User::factory()
             ->count(5)
             ->client()
-            ->has(Task::factory()->count(2)->create())
+            ->has(Task::factory()
+                    ->count(20)
+                    ->state(new Sequence(
+                        ['status' => TaskStatus::OPENED],
+                        ['status' => TaskStatus::PENDING],
+                    ))
+                    )
             ->create();
     }
 }
