512cdf0f1ef8661c1befbf0a9cebfd210a96b21b
Wesley-kami <okwuwesley@gmail.com>
2026-04-20 19:02:19 +0100

    feat- API health route

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/health', function(Request $request){
+    return [
+        "app_name"=> config('app.name'),
+        "version" => '1.0',
+        "health" => 'OK',
+    ];
+});
