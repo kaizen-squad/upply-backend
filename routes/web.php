87caf267dfa4aa1cb816b01dd1a27c739736c4ac
e-mandy <andymfrd02@gmail.com>
2026-04-21 13:09:48 +0000

    chore: Merged the seeders branch

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,3 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-<<<<<<< HEAD
 Require __DIR__ . '/api.php' ;
-=======
-Route::get('/', function () {
-    return view('welcome');
-});
->>>>>>> 17ff392 (feat(architecture): Set up the backend code base structure)
