ceb9daaddf043c42f8cf45cf4a862a5abfacc1ae
e-mandy <andymfrd02@gmail.com>
2026-04-23 11:52:28 +0000

    feat(task): Defined policies for the task

---
+++ app/Policies/TaskPolicy.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Policies;
+
+use App\Enums\TaskStatus;
+use App\Enums\UserRole;
+use App\Models\Task;
+use App\Models\User;
+
+class TaskPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function create(User $user): bool
+    {
+        return $user->role == UserRole::Client;
+    }
+
+    public function update(User $user, Task $task): bool
+    {
+        return (
+            $user->id == $task->client_id
+            && $task->status == TaskStatus::OPENED
+            && $user->role == UserRole::Client
+        );
+    }
+
+    public function delete(User $user, Task $task){
+        return (
+            $user->id == $task->client_id
+            && $task->status == TaskStatus::OPENED
+            && $user->role == UserRole::Client
+        );
+    }
+}
