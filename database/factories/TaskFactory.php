6ba8afd0e57e108edf783a7fdcba111ab8e19e47
e-mandy <andymfrd02@gmail.com>
2026-04-23 01:50:35 +0000

    feat(task): Added the task factory

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Enums\TaskStatus;
 use App\Models\Model;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,7 +20,27 @@ class TaskFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            "id" => fake()->uuid(),
+            "client_id" => User::factory(),
+            "title" => fake()->jobTitle(),
+            "description" => fake()->paragraph(),
+            "budget" => fake()->numberBetween(10000, 100000),
+            "deadline" => fake()->dateTimeBetween('now', '+3 months'),
+            "status" => TaskStatus::OPENED,
         ];
+    }
+
+    public function pending(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'status' => TaskStatus::PENDING
+        ]);
+    }
+
+    public function validated(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'status' => TaskStatus::DELIVERED
+        ]);
     }
 }
