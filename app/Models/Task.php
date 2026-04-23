4bce69633dae93370a29586bb2e2c546d731ee5d
e-mandy <andymfrd02@gmail.com>
2026-04-23 01:52:30 +0000

    feat(task): Added the task model with relationship

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use App\Enums\TaskStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'client_id',
+
+        'title',
+        'description',
+        'budget',
+        'deadline',
+        'status'
+    ];
+
+    protected $casts = [
+        'status' => TaskStatus::class
+    ];
+
+    public function client(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'client_id', 'id');
+    }
 }
