f92743da874f1d15100ffa0eb4077eac825b98e4
e-mandy <andymfrd02@gmail.com>
2026-04-23 22:37:40 +0000

    feat(deliverables): Set up the DTO for the submittion of a deliverable

--- app/DTOs/Deliverable/SubmitDeliverableDTO.php
+++ app/DTOs/Deliverable/SubmitDeliverableDTO.php
@@ -2,6 +2,8 @@
 
 namespace App\DTOs\Deliverable;
 
+use App\Http\Requests\Deliverable\SubmitDeliverableRequest;
+
 readonly class SubmitDeliverableDTO{
     public function __construct(
         public string $content,
@@ -9,7 +11,14 @@ readonly class SubmitDeliverableDTO{
         public string $task_id
     ){}
 
-    public static function fromRequest(){
-        //
+    public static function fromRequest(SubmitDeliverableRequest $request): self
+    {
+        $data = $request->validated();
+
+        return new self(
+            content: $data['content'],
+            file_path: $data['file_path'],
+            task_id: $data['task_id']
+        );
     }
 }
