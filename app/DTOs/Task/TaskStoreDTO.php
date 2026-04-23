9805d3eda3204b138a5d6f976538f7c64e960094
e-mandy <andymfrd02@gmail.com>
2026-04-23 11:37:49 +0000

    fix(task): Removed the status from the required request data for the DTO

--- app/DTOs/Task/TaskStoreDTO.php
+++ app/DTOs/Task/TaskStoreDTO.php
@@ -2,18 +2,15 @@
 
 namespace App\DTOs\Task;
 
-use App\Enums\TaskStatus;
 use App\Http\Requests\Task\TaskStoreRequest;
 use Carbon\Carbon;
 
 readonly class TaskStoreDTO{
     public function __construct(
-        public string $client_id,
         public string $title,
         public string $description,
         public int $budget,
         public Carbon $deadline,
-        public TaskStatus $status
     ){}
 
     public static function fromRequest(TaskStoreRequest $request): self
@@ -21,12 +18,10 @@ readonly class TaskStoreDTO{
         $data = $request->validated();
 
         return new self(
-            client_id: $data['client_id'],
             title: $data['title'],
             description: $data['description'],
             budget: $data['budget'],
-            deadline: Carbon::parse($data['deadline']),
-            status: TaskStatus::from($data['status'])
+            deadline: Carbon::parse($data['deadline'])
         );
     }
 }
