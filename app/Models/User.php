dfd68531b37280650b995426f64484bac75727b1
e-mandy <andymfrd02@gmail.com>
2026-04-23 05:36:58 +0000

    chore: Rebased the taskService branch on develop

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ namespace App\Models;
 use App\Traits\AuthToken;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -19,7 +20,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasUuids, HasFactory, Notifiable, HasApiTokens, AuthToken;
+    use HasUuids, HasFactory, Notifiable, HasApiTokens, AuthToken, SoftDeletes;
 
     public $incrementing = false;
     protected $keyType = 'string';
