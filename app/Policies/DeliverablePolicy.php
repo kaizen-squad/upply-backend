441b970fbd90595a0f49f3b44463139151037d56
e-mandy <andymfrd02@gmail.com>
2026-04-23 22:09:35 +0000

    feat(deliverables): Set up the policy for the submittion of a deliverable

---
+++ app/Policies/DeliverablePolicy.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+
+class DeliverablePolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function submit(){
+        
+    }
+}
---
+++ app/Services/DeliverableService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services;
+
+class DeliverableService{
+    public function submit(){
+        //
+    }
+}
