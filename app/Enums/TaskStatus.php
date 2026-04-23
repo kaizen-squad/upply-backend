a8208333c27b6a2f78093bc3e9a958aa155f435a
e-mandy <andymfrd02@gmail.com>
2026-04-23 01:51:49 +0000

    feat(task): Added the task status enum

---
+++ app/Enums/TaskStatus.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Enums;
+
+enum TaskStatus: string
+{
+    case OPENED = "OUVERTE";
+    case PENDING = "EN_COURS";
+    case DELIVERED = "LIVREE";
+    case VALIDATED = "VALIDEE";
+
+    public function isTerminal(): bool
+    {
+        return match($this){
+            self::VALIDATED => true,
+            self::OPENED, self::PENDING => false
+        };
+    }
+}
