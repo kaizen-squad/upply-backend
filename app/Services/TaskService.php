384f7cebd38084cae28fd83bf3cf61ec6ca91d00
e-mandy <andymfrd02@gmail.com>
2026-04-23 11:31:39 +0000

    feat(task): Set up the services methods

--- app/Services/TaskService.php
+++ app/Services/TaskService.php
@@ -2,8 +2,13 @@
 
 namespace App\Services;
 
+use App\DTOs\Task\TaskStoreDTO;
+use App\DTOs\Task\TaskUpdateDTO;
+use App\Enums\UserRole;
 use App\Http\Resources\TaskResource;
 use App\Models\Task;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 
 class TaskService{
 
@@ -13,7 +18,47 @@ class TaskService{
         return TaskResource::collection($tasks);
     }
 
-    public function create(){
-        //
+    public function create(User $client, TaskStoreDTO $newTask){
+        // Check the ability to create a task.
+        Gate::authorize('create', Task::class);
+
+        // Create the Task.
+        $createdTask = Task::create([
+            'title' => $newTask->title,
+            'description' => $newTask->description,
+            'budget' => $newTask->budget,
+            'deadline' => $newTask->deadline,
+            'status' => UserRole::Client,
+
+            'client_id' => $client->id
+        ]);
+
+        return new TaskResource($createdTask);
+    }
+
+    public function show(Task $task){
+        return new TaskResource($task->load('client'));
+    }
+
+    public function update(Task $targetTask, TaskUpdateDTO $data): bool
+    {
+        // Check the ability of the user to update this task.
+        Gate::authorize('update', $targetTask);
+
+        $isUpdated = $targetTask->update([
+            'title' => $data->title,
+            'description' => $data->description,
+            'budget' => $data->budget,
+            'deadline' => $data->deadline,
+        ]);
+
+        return $isUpdated;
+    }
+
+    public function delete(Task $task): bool
+    {
+        Gate::authorize('delete', $task);
+
+        return $task->delete();
     }
 }
