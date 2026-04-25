8e07320d0499b977ea8a5b9237d700cd190103f1
Wesley-kami <okwuwesley@gmail.com>
2026-04-25 22:08:42 +0100

    fix- Dockerfile reconfiguration

--- Docker/entry.sh
+++ Docker/entry.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
+set -e # Arrête le script en cas d'erreur
 
-if [ ! -f "vendor/autoload.php" ]; then
-    composer install --no-progress --no-interaction --no-dev
+# On ne fait plus de composer install ici, il est déjà dans l'image !
+
+if [ ! -f ".env" ]; then
+    cp .env.example .env
 fi
 
-echo "Waiting for database ($DB_HOST:$DB_PORT)..."
+# On attend la DB
 until pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USERNAME"; do
-    echo "Database is unavailable - sleeping"
+    echo "Waiting for database..."
     sleep 2
 done
-echo "Database is up!"
 
+# On lance les migrations
 php artisan migrate --force
+
+# Suppression des chown (inutiles/interdits sur bcp de PaaS en runtime)
+# Suppression des caches
 php artisan config:cache
 php artisan route:cache
-php artisan view:cache
 
-# Plus de chown ici
-exec frankenphp run --config /app/Docker/Caddyfile
+echo "🚀 Starting FrankenPHP..."
+
+# ✅ Utilisation du port dynamique de Render
+exec frankenphp run --config /app/Docker/Caddyfile --listen :$PORT
