067a8c5868024d4c533e2e048019aa676eb4885d
e-mandy <andymfrd02@gmail.com>
2026-04-26 17:21:32 +0000

    feat(rewiews): Finalized the note method controller

--- app/Http/Controllers/Api/Review/ReviewController.php
+++ app/Http/Controllers/Api/Review/ReviewController.php
@@ -2,11 +2,28 @@
 
 namespace App\Http\Controllers\Api\Review;
 
+use App\DTOs\Review\ReviewStoreDTO;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Review\ReviewStoreRequest;
+use App\Services\ReviewService;
 
 class ReviewController extends Controller
 {
-    public function note(){
-        
+    public function __construct(
+        public ReviewService $service
+    ){}
+
+    public function note(ReviewStoreRequest $request){
+        $user = $request->user();
+
+        $data = ReviewStoreDTO::fromRequest($request);
+
+        $response = $this->service->note($user, $data);
+
+        return response()->json([
+            "success" => true,
+            "data" => $response,
+            "message" => "Review created successfully"
+        ], 201);
     }
 }
