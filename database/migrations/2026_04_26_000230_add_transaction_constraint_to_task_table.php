69737096675215396e10afd4a21d9b6b8abbb885
e-mandy <andymfrd02@gmail.com>
2026-04-26 17:50:24 +0000

    feat(reviews): Tested note endpoints

--- database/migrations/2026_04_26_000230_add_transaction_constraint_to_task_table.php
+++ database/migrations/2026_04_26_000230_add_transaction_constraint_to_task_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::table('tasks', function (Blueprint $table) {
 <<<<<<< HEAD
+<<<<<<< HEAD
             $table->foreignUuid('transaction_id')->constrained();
 =======
             $table->foreignId('transaction_id')->constrained();
 >>>>>>> 1a96ffa (fix(endpoint): Added the deliverable validation endpoint)
+=======
+            $table->foreignUuid('transaction_id')->constrained();
+>>>>>>> 6c6097f (feat(reviews): Tested note endpoints)
         });
     }
 
