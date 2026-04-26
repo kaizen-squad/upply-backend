50495b2800b6dc0d4b6591fe78dd7c6b0e3fcc4c
e-mandy <andymfrd02@gmail.com>
2026-04-26 17:12:53 +0000

    feat(reviews): Set up the review json resource

---
+++ app/Http/Controllers/Api/Review/ReviewController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api\Review;
+
+use App\Http\Controllers\Controller;
+
+class ReviewController extends Controller
+{
+    public function note(){
+        
+    }
+}
