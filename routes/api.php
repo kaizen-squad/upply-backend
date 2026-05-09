8da0118be3edf6ab73d55720ef599c8b0d6b2b66
e-mandy <andymfrd02@gmail.com>
2026-05-09 03:13:29 +0000

    fix: Removed checked endpoint one time again

--- routes/api.php
+++ routes/api.php
@@ -57,7 +57,7 @@ Route::middleware('authentify')->group(function () {
 
     Route::post('/tasks/apply', [ApplicationController::class, 'apply']);
 
-    Route::get('/applications/mine', [ApplicationController::class, 'listMine']); // Validé
+    Route::get('/applications/mine', [ApplicationController::class, 'listMine']);
 
     Route::put('/application/{application}/accept', [ApplicationController::class, 'accept']);
 
