1042c4dedc0b36e9211c3fff1678f0d595b38b01
e-mandy <andymfrd02@gmail.com>
2026-05-09 07:31:12 +0000

    feat(dashboard): Defined access gate for each feature

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,14 @@
 
 namespace App\Providers;
 
+use App\Enums\UserRole;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Sanctum\Sanctum;
 use App\Models\PersonalAccessToken;
 use App\Models\Review;
+use App\Models\User;
 use App\Observers\ReviewObserver;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +28,13 @@ class AppServiceProvider extends ServiceProvider
     {
         Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
         Review::observe(ReviewObserver::class);
+
+        Gate::define('client-access-dashboard', function (User $user): bool {
+            return $user->role === UserRole::Client;
+        });
+
+        Gate::define('prestataire-access-dashboard', function (User $user): bool{
+            return $user->role !== UserRole::Prestataire;
+        });
     }
 }
---
+++ app/Services/DashboardService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+
+class DashboardService{
+    public function forClient(User $client){
+        
+    }
+}
