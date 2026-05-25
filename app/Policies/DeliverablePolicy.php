69e947046373f623f06467fb6c5392aca60182dc
e-mandy <andymfrd02@gmail.com>
2026-05-25 10:08:17 +0100

    fix(deliverables): Added the error message for the deliverable policy

--- app/Policies/DeliverablePolicy.php
+++ app/Policies/DeliverablePolicy.php
@@ -8,6 +8,7 @@ use App\Models\Application;
 use App\Models\Deliverable;
 use App\Models\Task;
 use App\Models\User;
+use Illuminate\Auth\Access\Response;
 
 class DeliverablePolicy
 {
@@ -19,43 +20,43 @@ class DeliverablePolicy
         //
     }
 
-    public function submit(User $user, Task $task): bool
+    public function submit(User $user, Task $task): Response
     {
-        if($user->role !== UserRole::Prestataire) return false;
+        if($user->role !== UserRole::Prestataire) return Response::deny("Vous devez être un prestataire pour éffectuer cette action.");
 
         $hasAcceptedApplication = Application::where('task_id', $task->id)
             ->where('prestataire_id', $user->id)
             ->where('status', ApplicationStatus::ACCEPTED)
             ->exists();
 
-        if(!$hasAcceptedApplication) return false;
+        if(!$hasAcceptedApplication) return Response::deny("Vous candidature doit être accepté au préalable.");
 
-        return true;
+        return Response::allow();
     }
 
-    public function get(User $user, Task $task): bool
+    public function get(User $user, Task $task): Response
     {
-        if($user->role !== UserRole::Client && $user->role !== UserRole::Prestataire) return false;
+        if($user->role !== UserRole::Client && $user->role !== UserRole::Prestataire) return Response::deny("Vous devez être un client ou un prestataire pour effectuer cette action.");
 
         $hasAcceptedApplication = Application::where('task_id', $task->id)
             ->where('prestataire_id', $user->id)
             ->where('status', ApplicationStatus::ACCEPTED)
             ->exists();
 
-        if($hasAcceptedApplication) return true;
+        if($hasAcceptedApplication) return Response::allow();
 
         $isTaskOwner = ($task->client_id === $user->id);
 
-        if($isTaskOwner) return true;
+        if($isTaskOwner) return Response::allow();
 
-        return false;
+        return Response::deny("Vous ne pouvez pas voir ce livrable. Vous n'avez pas les permissions requises.");
     }
 
-    public function validate(User $user, Deliverable $deliverable): bool
+    public function validate(User $user, Deliverable $deliverable): Response
     {
         return (
             $user->id === $deliverable->task->client_id
             && $user->role === UserRole::Client
-        );
+        ) ? Response::allow() : Response::deny("Seul le client propriétaire de cette mission peut valider le livrable.");
     }
 }
