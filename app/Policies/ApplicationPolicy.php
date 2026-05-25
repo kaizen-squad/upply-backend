baaa71941d6f7e575217640c05c735431809beb1
e-mandy <andymfrd02@gmail.com>
2026-05-25 10:02:09 +0100

    feat(application): Added the message for the application policy

--- app/Policies/ApplicationPolicy.php
+++ app/Policies/ApplicationPolicy.php
@@ -6,6 +6,7 @@ use App\Enums\UserRole;
 use App\Models\Application;
 use App\Models\Task;
 use App\Models\User;
+use Illuminate\Auth\Access\Response;
 
 class ApplicationPolicy
 {
@@ -17,49 +18,50 @@ class ApplicationPolicy
         //
     }
 
-    public function create(User $user, Task $task): bool
+    public function create(User $user, Task $task): Response
     {
         return (
             $user->id !== $task->client_id
             && $user->role === UserRole::Prestataire
-        );
+        ) ? Response::allow() : Response::deny("Vous devez être un client pour créer des missions.");
     }
 
-    public function listForTask(User $user, Task $task): bool
+    public function listForTask(User $user, Task $task): Response
     {
         return (
             $user->role === UserRole::Client
             && $user->id == $task->client_id
-        );
+        ) ? Response::allow() : Response::deny("Seul le client propriétaire de cette mission peut voir les candidatures associées.");
     }
 
-    public function currentApplication(User $user, Task $task){
+    public function currentApplication(User $user, Task $task): Response
+    {
         return (
             $user->role === UserRole::Prestataire
             && Application::where('prestataire_id', $user->id)
             ->where('task_id', $task->id)
             ->exists()
-        );
+        ) ? Response::allow() : Response::deny("Seul le prestataire propriétaire de cette candidature peut la voir.");
     }
 
-    public function listMine(User $user): bool
+    public function listMine(User $user): Response
     {
-        return $user->role === UserRole::Prestataire;
+        return ($user->role === UserRole::Prestataire) ? Response::allow() : Response::deny("Seul le client propriétaire de ces candidatures peut les voir.");
     }
 
-    public function accept(User $user, Task $task): bool
-    {
-        return (
-            $user->id === $task->client_id
-            && $user->role === UserRole::Client
-        );
-    }
-
-    public function reject(User $user, Task $task): bool
+    public function accept(User $user, Task $task): Response
     {
         return (
             $user->id === $task->client_id
             && $user->role === UserRole::Client
-        );
+        ) ? Response::allow() : Response::deny("Seul le client propriétaire de cette mission peut accepter la candidature.");
+    }
+
+    public function reject(User $user, Task $task): Response
+    {
+        return (
+            $user->id === $task->client_id
+            && $user->role === UserRole::Client
+        ) ? Response::allow() : Response::deny("Seul le client propriétaire de cette mission peut rejeter la candidature.");
     }
 }
